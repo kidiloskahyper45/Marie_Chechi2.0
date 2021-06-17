a4f4d795721a52f9c33a980b485af45cc8ebd01e
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-06-17 12:29:40 +0530

    Update disable.py

--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -146,7 +146,7 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat_id)
 
 
-    __mod_name__ = "Command disabling"
+    __mod_name__ = "Command disabling👔"
 
     __help__ = """
  - /cmds: Disabled ചെയ്ത കമാൻഡുകളുടെ നിലവിലെ നില പരിശോധിക്കുക.
